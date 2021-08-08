09d104df0281cbb95bbb301523bf395d489d7ea0
Igor_Alexandrovich <i_swat@mail.ru>
2021-08-08 16:35:34 +0300

    3. Представление и модель представления

--- script/main.js
+++ script/main.js
@@ -4,14 +4,17 @@ let webStore = new Vue({
 	el: '#app',
 	data: {
 		siteName: 'Сайт Vue.JS по книге VueJS в действии',
+		showProduct: true,
 		product: {
 			id: 1001,
 			title: 'Еда для кошки, 25кг. большая упаковка',
 			description: 'Большая упаковка 25кг <em>очень полезная</em>, органическая еда для вашего кота',
 			price: 2000,
 			priceFilter: '',
-			image: 'images/product-fullsize.png'
-		}
+			image: 'images/product-fullsize.png',
+			availableInventory: 5
+		},
+		cart: []
 	},
 	filters: {
 		formatPrice(price) {	//#B
@@ -28,6 +31,22 @@ let webStore = new Vue({
 			} else {
 				return "$" + (price / 100).toFixed(2);	//#H
 			}
+		}
+	},
+	methods: {
+		addToCart() {
+			this.cart.push( this.product.id );
+		},
+		showCheckout() {
+			this.showProduct = this.showProduct ? false : true;
+		}
+	},
+	computed: {
+		cartItemCount() {
+			return this.cart.length || 0;
+		},
+		canAddToCart() {
+			return this.product.availableInventory > this.cartItemCount;
 		}
 	}
 });
